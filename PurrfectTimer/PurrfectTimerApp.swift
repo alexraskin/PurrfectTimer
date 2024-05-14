5148ac5f004da83869a1416657a496d10d86a6b1
alexraskin <alexraskin@fastmail.fm>
2024-05-13 21:58:55 -0700

    Initial Commit

---
+++ PurrfectTimer/PurrfectTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrfectTimerApp.swift
+//  PurrfectTimer
+//
+//  Created by Alexander Raskin on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PurrfectTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
